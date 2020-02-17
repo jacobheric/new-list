72f33f6a07da9fe080adea95dd74b79285c392e2
Jacob Heric <jacob@jacobheric.com>
2020-02-17 15:13:37 -0500

    cleanup logging

--- src/server/graphql.ts
+++ src/server/graphql.ts
@@ -1,6 +1,5 @@
 import { gql } from "apollo-server-express";
 import { Notes } from "../config";
-import { tag } from "../client/util";
 
 export const typeDefs = gql`
 type Note {
@@ -30,12 +29,10 @@ export const resolvers = {
       notes: async (): Promise<any> => await Notes.findAll()
    },
    Mutation: {
-      addNote: async (root: any, args: any) => {
-         console.log('args', args);
-         //
-         // returned upserts are not supported with
-         // sqlite so just echo back the input
-         return Notes.upsert(args.note).then(() => args.note);
-      },
+      //
+      // returned upserts are not supported with
+      // sqlite so just echo back the input
+      addNote: async (root: any, args: any) =>
+         Notes.upsert(args.note).then(() => args.note)
    }
 };
