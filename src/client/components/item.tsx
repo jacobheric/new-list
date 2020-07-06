69f7dfa436dc5604dffbf1879d83df5bfab15728
Jacob Heric <jacob@jacobheric.com>
2020-07-06 15:20:37 -0400

    Break out list items to separate component

---
+++ src/client/components/item.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import {
+  FunctionComponent,
+  SyntheticEvent,
+  useContext,
+  useEffect,
+} from "react";
+import { ADD_NOTE, cacheUpdate, Note } from "./input";
+import classNames from "classnames";
+import * as r from "ramda";
+import { useMutation } from "@apollo/client";
+import { ErrorActionContext } from "./error";
+
+const ItemComponent: FunctionComponent<{
+  item: Note;
+  setInput: (note: Note) => void;
+}> = ({ item, setInput }) => {
+  const { addError } = useContext(ErrorActionContext);
+  const [addNote, { error: mutationError }] = useMutation(
+    ADD_NOTE,
+    cacheUpdate
+  );
+
+  useEffect(() => {
+    if (mutationError) {
+      addError(mutationError.message);
+    }
+  }, [mutationError]);
+  //
+  // update a note
+  const update = (e: SyntheticEvent, note: Note) => {
+    e.stopPropagation();
+    addNote({ variables: { note: r.omit(["__typename"], note) } });
+  };
+
+  const edit = (note: Note) => {
+    setInput(r.omit(["__typename"], note));
+  };
+
+  return (
+    <>
+      <li className="row px-1" key={item.uuid} onClick={() => edit(item)}>
+        <div
+          className={classNames("w-full", "cursor-pointer", {
+            strike: item.done
+          })}>
+          {item.note}
+        </div>
+        <div
+          className="cursor-pointer px-2 text-xl"
+          onClick={e => update(e, { ...item, ...{ archived: true } })}>
+          ⓧ
+        </div>
+        <div
+          className="cursor-pointer px-2 text-xl"
+          onClick={e => update(e, { ...item, ...{ done: !item.done } })}>
+          ✓
+        </div>
+      </li>
+      <hr key={`h${item.uuid}`} />
+    </>
+  );
+};
+export default ItemComponent;
--- src/client/components/list.tsx
+++ src/client/components/list.tsx
@@ -1,19 +1,12 @@
 import * as React from "react";
-import { ChangeEvent, SyntheticEvent, useContext, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import * as r from "ramda";
-import InputComponent, {
-  ADD_NOTE,
-  cacheUpdate,
-  mergeNote,
-  Note
-} from "./input";
-import { gql, useMutation, useQuery, useSubscription } from "@apollo/client";
+import InputComponent, { cacheUpdate, mergeNote, Note } from "./input";
+import { gql, useQuery, useSubscription } from "@apollo/client";
 import { gqlClient } from "../graphqlClient";
 import { feature } from "../../config";
 import { DataContainer } from "../util";
-import { ErrorActionContext } from "./error";
-import { useEffect } from "react";
-import classNames from "classnames";
+import ItemComponent from "./item";
 
 export const GET_NOTES = gql`
   query getNotes {
@@ -39,13 +32,8 @@ const NOTE_SUB = gql`
 
 const ListComponent = () => {
   const { loading, error, data } = useQuery(GET_NOTES);
-  const [addNote, { error: mutationError }] = useMutation(
-    ADD_NOTE,
-    cacheUpdate
-  );
-  const [input, setInput] = useState();
+  const [input, setInput] = useState<Note>();
   const [search, setSearch] = useState("");
-  const { addError } = useContext(ErrorActionContext);
 
   if (feature.realTime) {
     const sub = useSubscription(NOTE_SUB);
@@ -54,16 +42,6 @@ const ListComponent = () => {
     }
   }
 
-  useEffect(() => {
-    if (mutationError) {
-      addError(mutationError.message);
-    }
-  }, [mutationError]);
-
-  const edit = (note: Note) => {
-    setInput(r.omit(["__typename"], note));
-  };
-
   //
   // prettier-ignore
   const filter = () =>
@@ -71,13 +49,6 @@ const ListComponent = () => {
        (item: Note) =>
           r.includes(search.toLowerCase(), item.note.toLowerCase()),
        data.notes);
-
-  //
-  // update a note
-  const update = (e: SyntheticEvent, note: Note) => {
-    e.stopPropagation();
-    addNote({ variables: { note: r.omit(["__typename"], note) } });
-  };
 
   return (
     <DataContainer error={error ? error.message : undefined} loading={loading}>
@@ -96,34 +67,10 @@ const ListComponent = () => {
       <InputComponent note={input} />
       <ul>
         {data &&
-          filter().map((item: Note, i: number) =>
-            item.archived
-              ? null
-              : [
-                  <li className="row px-1" key={i} onClick={() => edit(item)}>
-                    <div
-                      className={classNames("w-full", "cursor-pointer", {
-                        strike: item.done
-                      })}>
-                      {item.note}
-                    </div>
-                    <div
-                      className="cursor-pointer px-2 text-xl"
-                      onClick={e =>
-                        update(e, { ...item, ...{ archived: true } })
-                      }>
-                      ⓧ
-                    </div>
-                    <div
-                      className="cursor-pointer px-2 text-xl"
-                      onClick={e =>
-                        update(e, { ...item, ...{ done: !item.done } })
-                      }>
-                      ✓
-                    </div>
-                  </li>,
-                  <hr key={`h${i}`} />
-                ]
+          filter().map((item: Note) =>
+            item.archived ? null : (
+              <ItemComponent item={item} setInput={setInput} />
+            )
           )}
       </ul>
     </DataContainer>
